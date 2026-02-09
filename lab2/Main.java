a302eaf296488b2c3ed0632705f0f89577212aad
Katie Broida <kbroida@gmail.com>
2026-02-09 14:23:58 -0500

    List methods

--- lab2/Main.java
+++ lab2/Main.java
@@ -1,10 +1,8 @@
 import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.Iterator;
+import java.util.ListIterator;
 
 // Lab 2 - Lists, Stacks, and Queues
-// Your Name: 
+// Your Name: Katie Broida
 // Notes: The instructions are found in comments through the code starting with "Step n"
 //        Please leave the comments and add your code just after the comment.
 //        Use the examples in the book to complete the lab.
@@ -15,25 +13,35 @@ class Main {
     // Comment/Uncomment the following 3 method calls as you work on each method.
     // No code should be added to the main method. All of your code will go into the methods in Lab2.
     lab.linkedList();
-    lab.queue();
-    lab.stack();
+    // lab.queue();
+    // lab.stack();
   }
 }
 
 class Lab2 {
   public void linkedList() {
     // 1. Create a LinkedList<String> object called progLanguages
+    LinkedList<String> progLanguages = new LinkedList<String>();
 
     // 2. Add elements to progLanguages: "Java", "Python", "JavaScript", "C++"
+    progLanguages.add("Java");
+    progLanguages.add("Python");
+    progLanguages.add("JavaScript");
+    progLanguages.add("C++");
 
     // 3. Remove the element "C++" from the list using .remove()
+    progLanguages.remove("C++");
 
     // 4. Add an element "HTML" at index 2.
+    progLanguages.add(2, "HTML");
 
     // 5. Iterate over progLanguages and use println() to output each element. You must create an Iterator<string> and use hasNext(), and next() similar to the example in our book.
-    
+    ListIterator<String> listIt = progLanguages.listIterator();
+    while (listIt.hasNext()) {
+      System.out.println(listIt.next() + " ");
+    }
   }
-  
+
   public void queue() {
     // 6. Create a Queue<String> called q
 
@@ -49,7 +57,7 @@ class Lab2 {
 
     // 11. Using for(String element : q), output all of the values in the queue
 
-    
+
   }
 
   public void stack() {
@@ -70,8 +78,8 @@ class Lab2 {
     // 19. Call empty(). Output the results
 
     // 20. Print the titles of all of the books on the stack
-    
+
   }
 
-  
+
 }
