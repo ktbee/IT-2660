c1ce69ffcf969972d4df329acbbeba7e522dec4e
Katie Broida <kbroida@gmail.com>
2026-02-20 13:56:16 -0500

    Timers for best and worst case scenarios

--- lab3/Main.java
+++ lab3/Main.java
@@ -1,30 +1,40 @@
 public class Main {
   public static void main(String[] args) {
     String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    
-    String bestCase = "";
+
+    String bestCase = "A";
 
     // * Initialize startTime here
+    long startTime = System.nanoTime();
     int index = match(text, bestCase);
     // * Initialize endTime here
+    long endTime = System.nanoTime();
     // * Calculate elapsedTime here
-    
+    long bestElapsedTime = endTime - startTime;
+
+    System.err.println("Best case elapsed time: " + bestElapsedTime + " nanoseconds");
+
     if (index >= 0)
       System.out.println("best-case input matched at index " + index);
     else
-      System.out.println("best-case input unmatched");   
+      System.out.println("best-case input unmatched");
 
-    String worstCase = "";
-    
+    String worstCase = "9";
+
     // * Initialize startTime here
+    startTime = System.nanoTime();
     index = match(text, worstCase);
     // * Initialize endTime here
+    endTime = System.nanoTime();
     // * Calculate elapsedTime here
-    
+    long worstElapsedTime = endTime - startTime;
+
+    System.err.println("Worst case elapsed time: " + worstElapsedTime + " nanoseconds");
+
     if (index >= 0)
       System.out.println("worst-case input matched at index " + index);
     else
-      System.out.println("worst-case input unmatched");  
+      System.out.println("worst-case input unmatched");
   }
 
   // Return the index of the first match. -1 otherwise.
@@ -36,16 +46,16 @@ public class Main {
 
     return -1;
   }
-	
+
   // Test if pattern matches text starting at index i
-  private static boolean isMatched(int i, String text, 
+  private static boolean isMatched(int i, String text,
       String pattern) {
     for (int k = 0; k < pattern.length(); k++) {
       if (pattern.charAt(k) != text.charAt(i + k)) {
         return false;
       }
     }
-    
+
     return true;
   }
 }
